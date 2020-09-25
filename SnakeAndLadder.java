d52b5f8b1604a6e8c81016b151edf96f0183facb
sharale34 <sourabhharale8000@gmail.com>
2020-09-25 08:52:35 +0530

    Added: Player Start Position is Added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,7 +4,9 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Let's Play A Game");	
+		//variables
+		int position=0;
+		System.out.println("Let's Play A Game.");
+		System.out.println("Current position of a player :"+position);
 	}
-
 }
