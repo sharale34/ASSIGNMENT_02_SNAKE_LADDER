1cfdaddb76a459bf013dec48f31eff8ea93fbb21
sharale34 <sourabhharale8000@gmail.com>
2020-09-25 08:41:44 +0530

    Added: Let's Play Message is Added.

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.SnakeLadder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Let's Play A Game");	
+	}
+
+}
